3162ba978bd4a9f3d17a6be218b788c7ac3c7499
Harsh Shah <73690892+Harsh-H-Shah@users.noreply.github.com>
2022-03-19 02:28:07 +0530

    Time: 62 ms (44.89%), Space: 14 MB (37.02%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,28 +5,31 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # sentinel
-        sentinel = ListNode(0, head)
-
-        # predecessor = the last node 
-        # before the sublist of duplicates
-        pred = sentinel
-        
-        while head:
-            # if it's a beginning of duplicates sublist 
-            # skip all duplicates
-            if head.next and head.val == head.next.val:
-                # move till the end of duplicates sublist
-                while head.next and head.val == head.next.val:
-                    head = head.next
-                # skip all duplicates
-                pred.next = head.next 
-            # otherwise, move predecessor
+        if not head:
+            return head
+        slow = ListNode()
+        slow.next = head
+        point = slow
+        fast = head
+        while(fast and fast.next):
+            if fast.val==fast.next.val:
+                val = fast.val
+                while fast and fast.val==val:
+                    fast.val = -101
+                    fast = fast.next
             else:
-                pred = pred.next 
-                
-            # move forward
-            head = head.next
-            
-        return sentinel.next
+                fast = fast.next
+        fast = head
+        while(fast and fast.next):
+            if fast.val!=-101:
+                slow.next = fast
+                fast = fast.next
+                slow = slow.next
+            else:
+                fast = fast.next
+        if fast.val==-101:
+            slow.next = None
+        else:
+            slow.next = fast
+        return point.next
                 
