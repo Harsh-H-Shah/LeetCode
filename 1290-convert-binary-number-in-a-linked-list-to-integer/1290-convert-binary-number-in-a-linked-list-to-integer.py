0fd8d3c87d806a9129c63d52a7423e702172984d
Harsh Shah <73690892+Harsh-H-Shah@users.noreply.github.com>
2022-03-19 13:48:16 +0530

    Time: 53 ms (28.07%), Space: 13.8 MB (65.34%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -5,15 +5,8 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        num = []
-        while(head):
-            num.append(head.val)
+        num = head.val
+        while head.next:
+            num = num * 2 + head.next.val
             head = head.next
-        ans = 0
-        num.reverse()
-        print(num)
-        if num[0] == 1:
-            ans+=1
-        for i in range(1,len(num)):
-            ans += (num[i]*2)**i
-        return ans
+        return num
