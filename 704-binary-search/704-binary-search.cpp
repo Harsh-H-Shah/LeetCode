9c3ba9a976654d19c8b46d41e759c1fb110442f6
Harsh Shah <73690892+Harsh-H-Shah@users.noreply.github.com>
2022-04-14 08:13:53 +0530

    Time: 40 ms (78.74%), Space: 27.7 MB (12.68%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size();
-        int mid = left + (right-left)/2;
-        while(left<right){
-            mid = left + (right-left)/2;
-            if (nums.at(mid)==target){
-                return mid;
-            }
-            else if(nums.at(mid)<target){
-                left = mid + 1;
-            }
-            else{
-                right = mid;
-            }
-        }
-        return -1;
+        int pivot, left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (nums[pivot] == target) return pivot;
+      if (target < nums[pivot]) right = pivot - 1;
+      else left = pivot + 1;
+    }
+    return -1;
     }
 };
