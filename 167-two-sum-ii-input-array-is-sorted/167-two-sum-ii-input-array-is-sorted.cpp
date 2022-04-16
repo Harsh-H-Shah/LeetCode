2972c77a08e874709ef083fabf967a3f77620ddd
Harsh Shah <73690892+Harsh-H-Shah@users.noreply.github.com>
2022-04-16 13:43:18 +0530

    Time: 13 ms (76.80%), Space: 15.7 MB (5.92%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,10 +4,9 @@ public:
         int l=0,r=numbers.size()-1;
     while(l<=r)
     {
-        int sum = numbers[l]+numbers[r];
-        if(sum == target)
+        if(numbers[l]+numbers[r] == target)
             return {l+1,r+1};
-        if(sum > target)
+        if(numbers[l]+numbers[r] > target)
             r--;
         else
             l++;
